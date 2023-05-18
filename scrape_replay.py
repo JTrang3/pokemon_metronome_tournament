d35a8ca9ce03306b88716aeaf2196ada17286ff1
JT <jefftrang3@gmail.com>
2023-05-18 03:23:15 -0700

    added transfer_metronome_data() function
    - stores every move collected in dict into spreadsheet

--- scrape_replay.py
+++ scrape_replay.py
@@ -1,8 +1,9 @@
 # To-Do List:
 # 1) Parse team names from usernames & get rosters - NOT STARTED
-# 2) Collect all moves used by Metronome from battle logs to database - IN PROGRESS
+# 2) Collect all moves used by Metronome from battle logs to database - DONE
+# 3) Connect moves with corresponding type, power, accuracy (include priority & description?) - IN PROGRESS
 
-import re
+import re, openpyxl
 
 # Gather all the moves used by Metronome in the entire battle
 def metronome_data(logs):
@@ -19,6 +20,36 @@ def metronome_data(logs):
             moves_used[move] = moves_used.get(move, 0) + 1
     return moves_used
 
+# Send move data to Excel spreadsheet
+def transfer_metronome_data(dict, xl_file, sheetname):
+    # Load the existing workbook & worksheet
+    workbook = openpyxl.load_workbook(xl_file)
+    worksheet = workbook[sheetname]
+    # Write the dictionary contents to the worksheet
+    # row = worksheet.max_row + 1
+    # for key, value in dict.items():
+    #     worksheet[f"A{row}"] = key
+    #     worksheet[f"B{row}"] = value
+    #     row += 1
+    for key, value in dict.items():
+        # Flag to check if the move exists in the worksheet
+        move_exists = False  
+        # Search for the move in the existing data
+        for row_index, row in enumerate(worksheet.iter_rows(min_row=2, max_row=worksheet.max_row, values_only=True), start=2):
+            if row[0] == key:
+                # Move already exists, increase the count
+                move_exists = True
+                worksheet.cell(row=row_index, column=2).value += value
+                break
+        if move_exists == False:
+            # Move doesn't exist, add it to the next available row
+            next_row = worksheet.max_row + 1
+            worksheet[f"A{next_row}"] = key
+            worksheet[f"B{next_row}"] = value
+    # Save the workbook
+    workbook.save(xl_file)
+    return
+
 def get_teams_and_roster():
     return
 
@@ -28,9 +59,11 @@ def main():
     moves = metronome_data(battle_log)
     
     # TEST: Count each move used & total moves used in a battle
-    for move, count in moves.items():
-        print(f"{move}: {count}")
-    print(f"Keys: {len(moves.keys())}, Values: {sum(moves.values())}")
+    # for move, count in moves.items():
+    #     print(f"{move}: {count}")
+    # print(f"Keys: {len(moves.keys())}, Values: {sum(moves.values())}")
+
+    transfer_metronome_data(moves, "tournament_statistics.xlsx", "Metronome_Data (2)")
 
 if __name__ == "__main__":
     main()
